54f431a4184ec3b06a9107af7d24dc173a3ca38f
Amr Elsayed <amr-elsayed@outlook.com>
2024-08-30 01:17:31 +0300

    add basic files
    routes, controllers, resources, views

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -16,13 +16,19 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <h1>TaskPro</h1>
                                 </Link>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                                <NavLink href={route('projects.index')} active={route().current('dashboard.index')}>
+                                    Projects
+                                </NavLink>
+                                <NavLink href={route('tasks.index')} active={route().current('tasks.index')}>
+                                    Tasks
                                 </NavLink>
                             </div>
                         </div>
